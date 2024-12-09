4a9e649cbd17c5caaf9734fe3dba6a11ad39129a
Minja Ćućula <cuculaminja@proton.me>
2024-12-09 18:38:14 +0100

    Add empty board

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import { Board } from "./types";
+import boardsSlice from "./redux/boardsSlice";
 import Header from "./components/Header";
+import Center from "./components/Center";
+import EmptyBoard from "./components/EmptyBoard";
 
 function App() {
+  const dispatch = useDispatch();
   const [boardModalOpen, setBoardModalOpen] = useState(false);
 
+  const boards = useSelector((state: { boards: Board[] }) => state.boards);
+  const activeBoard = boards.find((board: Board) => board.isActive);
+
+  if (!activeBoard && boards.length > 0)
+    dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
+
   return (
-    <div>
-      <Header
-        boardModalOpen={boardModalOpen}
-        setBoardModalOpen={setBoardModalOpen}
-      />
+    <div className="overflow-hidden overflow-x-scroll">
+      <>
+        {boards.length > 0 ? (
+          <>
+            <Header
+              boardModalOpen={boardModalOpen}
+              setBoardModalOpen={setBoardModalOpen}
+            />
+            <Center />
+          </>
+        ) : (
+          <>
+            <EmptyBoard type="add" />
+          </>
+        )}
+      </>
     </div>
   );
 }
