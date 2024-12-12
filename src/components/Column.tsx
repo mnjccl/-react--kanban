3a34a93176e078b079b7aa0cec2614acb9c40365
Minja Ćućula <cuculaminja@proton.me>
2024-12-12 18:47:02 +0100

    Add columns to main component

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,4 +1,24 @@
-function Column() {
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function Column({ colIndex }: { colIndex: number }) {
+  const colors = [
+    "bg-red-500",
+    "bg-orange-500",
+    "bg-blue-500",
+    "bg-purple-500",
+    "bg-green-500",
+    "bg-indigo-500",
+    "bg-yellow-500",
+    "bg-pink-500",
+    "bg-sky-500",
+  ];
+  const [color, setColor] = useState("");
+
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const board = boards.find((board) => board.isActive);
+  const col = board;
   return <div>Column</div>;
 }
 
